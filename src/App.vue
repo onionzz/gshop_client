bfd8d4493cf831da3b488460f1b696b939c04dc6
jenkins <zhangjie@benlai.com>
2019-09-19 10:13:48 +0800

    init git

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <footer-guide v-show="$route.meta.showFooter"></footer-guide>
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+  import {reqCategorys} from './api'
+
+  export default {
+    name: 'App',
+    components: {
+      FooterGuide
+    },
+    async mounted () {
+      const result = await reqCategorys()
+      console.log(result)
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    width 100%
+    height 100%
+    background #f5f5f5
+</style>
